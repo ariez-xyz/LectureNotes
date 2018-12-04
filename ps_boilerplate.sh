95cb02b17b0601b5be313c283407b2ddd48a45e3
David <dpape39@icloud.com>
2018-12-04 09:45:13 +0100

    add swe ps

--- ps_boilerplate.sh
+++ ps_boilerplate.sh
@@ -2,12 +2,6 @@ DATE=$(date "+%d-%m-%Y")-PS
 
 echo \# $DATE > $DATE.md
 echo >> $DATE.md
-echo "<!-- Mermaid diagram support... add diags via <<{$FILE} -->" >>$DATE.md
-echo "<script src="../html/mermaid.min.js"></script>" >> $DATE.md
-echo "<script src="../html/removeDiff.js"></script>" >> $DATE.md
-echo >> $DATE.md
 echo \<\!--TOC--\> >> $DATE.md
 echo >> $DATE.md
 echo \#\# >> $DATE.md
-echo >> $DATE.md
-echo "<script src="../html/removeCaptions.js"></script>" >> $DATE.md
